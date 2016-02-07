3d9b1cdccdf880e959351a8f71864078a5f8140d
Mihai Dima <mihaizn@gmail.com>
2016-02-07 18:19:45 +0200

    implement extended query list action

--- store-query.js
+++ store-query.js
@@ -5,11 +5,11 @@ var Uuid = require('node-uuid')
 var RelationalStore = require('./lib/relational-util')
 var QueryBuilder = require('./lib/query-builder')
 
-module.exports = function user (options) {
+module.exports = function queryBuilder (options) {
   var seneca = this
-  var store = seneca.store
+  var storeName = 'postgresql-store'
 
-  // seneca.add({role: store.name, hook: 'save'}, function (args, done) {
+  // seneca.add({role: storeName, hook: 'save'}, function (args, done) {
   //   var ent = args.ent
   //   var query
   //   var update = !!ent.id
@@ -19,7 +19,7 @@ module.exports = function user (options) {
   //     execQuery(query, function (err, res) {
   //       if (error(query, args, err)) {
   //         seneca.log.error(query.text, query.values, err)
-  //         return done({code: 'update', tag: args.tag$, store: store.name, query: query, error: err})
+  //         return done({code: 'update', tag: args.tag$, store: storeName, query: query, error: err})
   //       }
 
   //       seneca.log(args.tag$, 'update', ent)
@@ -34,7 +34,7 @@ module.exports = function user (options) {
   //     execQuery(query, function (err, res) {
   //       if (error(query, args, err)) {
   //         seneca.log.error(query.text, query.values, err)
-  //         return done({code: 'save', tag: args.tag$, store: store.name, query: query, error: err})
+  //         return done({code: 'save', tag: args.tag$, store: storeName, query: query, error: err})
   //       }
 
   //       seneca.log(args.tag$, 'save', ent)
@@ -43,20 +43,20 @@ module.exports = function user (options) {
   //   }
   // })
 
-  // seneca.add({role: store.name, hook: 'load'}, function (args, done) {
+  // seneca.add({role: storeName, hook: 'load'}, function (args, done) {
   //   var qent = args.qent
   //   var q = args.q
 
   //   QueryBuilder.selectstm(qent, q, function (err, query) {
   //     if (err) {
-  //       return done({code: 'load', tag: args.tag$, store: store.name, query: query, error: err})
+  //       return done({code: 'load', tag: args.tag$, store: storeName, query: query, error: err})
   //     }
 
   //     execQuery(query, function (err, res) {
   //       if (error(query, args, err)) {
   //         var trace = new Error()
   //         seneca.log.error(query.text, query.values, trace.stack)
-  //         return done({code: 'load', tag: args.tag$, store: store.name, query: query, error: err})
+  //         return done({code: 'load', tag: args.tag$, store: storeName, query: query, error: err})
   //       }
 
   //       var ent = null
@@ -70,67 +70,50 @@ module.exports = function user (options) {
   //   })
   // })
 
-  seneca.add({role: store.name, hook: 'list'}, function (args, done) {
+  seneca.add({role: storeName, hook: 'list'}, function (args, done) {
     var qent = args.qent
     var q = args.q
 
     var list = []
 
     buildSelectStatement(q, function (err, query) {
-      if (err) {
-        seneca.log.error('Postgres list error', err)
-        return done({code: 'list', tag: args.tag$, store: store.name, query: q, error: err})
-      }
-
-      execQuery(query, function (err, res) {
-        if (error(query, args, err, done)) {
-          return done({code: 'list', tag: args.tag$, store: store.name, query: query, error: err})
-        }
-
-        res.rows.forEach(function (row) {
-          var attrs = internals.transformDBRowToJSObject(row)
-          var ent = RelationalStore.makeent(qent, attrs)
-          list.push(ent)
-        })
-        seneca.log(args.tag$, 'list', list.length, list[0])
-        return done(null, list)
-      })
+      return done(err, {query: query})
     })
 
-  //   function buildSelectStatement (q, done) {
-  //     var query
+    function buildSelectStatement (q, done) {
+      var query
 
-  //     if (_.isString(q)) {
-  //       return done(null, q)
-  //     }
-  //     else if (_.isArray(q)) {
-  //       // first element in array should be query, the other being values
-  //       if (q.length === 0) {
-  //         var errorDetails = {
-  //           message: 'Invalid query',
-  //           query: q
-  //         }
-  //         seneca.log.error('Invalid query')
-  //         return done(errorDetails)
-  //       }
-  //       query = {}
-  //       query.text = QueryBuilder.fixPrepStatement(q[0])
-  //       query.values = _.clone(q)
-  //       query.values.splice(0, 1)
-  //       return done(null, query)
-  //     }
-  //     else {
-  //       if (q.ids) {
-  //         return done(null, QueryBuilder.selectstmOr(qent, q))
-  //       }
-  //       else {
-  //         QueryBuilder.selectstm(qent, q, done)
-  //       }
-  //     }
-  //   }
-  // })
+      if (_.isString(q)) {
+        return done(null, q)
+      }
+      else if (_.isArray(q)) {
+        // first element in array should be query, the other being values
+        if (q.length === 0) {
+          var errorDetails = {
+            message: 'Invalid query',
+            query: q
+          }
+          seneca.log.error('Invalid query')
+          return done(errorDetails)
+        }
+        query = {}
+        query.text = QueryBuilder.fixPrepStatement(q[0])
+        query.values = _.clone(q)
+        query.values.splice(0, 1)
+        return done(null, query)
+      }
+      else {
+        if (q.ids) {
+          return done(null, QueryBuilder.selectstmOr(qent, q))
+        }
+        else {
+          QueryBuilder.selectstm(qent, q, done)
+        }
+      }
+    }
+  })
 
-  // seneca.add({role: store.name, hook: 'remove'}, function (args, done) {
+  // seneca.add({role: storeName, hook: 'remove'}, function (args, done) {
   //   var qent = args.qent
   //   var q = args.q
 
