e7c0ecfe08a28954b5240ba39564a43899cc014f
Mihai Dima <mihaizn@gmail.com>
2016-03-14 13:52:00 +0200

    extended query also supports mysql

--- store-query.js
+++ store-query.js
@@ -23,40 +23,6 @@ module.exports = function queryBuilder (options) {
     return sTypes
   }
 
-  seneca.add({role: actionRole, hook: 'save'}, function (args, done) {
-    var ent = args.ent
-    var update = !!ent.id
-    var query
-    var autoIncrement = args.auto_increment || false
-    var sTypes = specificTypes(args.target)
-
-    if (update) {
-      query = QueryBuilder.updatestm(ent, sTypes)
-      return done(null, {query: query, operation: 'update'})
-    }
-
-    if (ent.id$) {
-      ent.id = ent.id$
-      query = QueryBuilder.savestm(ent, sTypes)
-      return done(null, {query: query, operation: 'save'})
-    }
-
-    if (autoIncrement) {
-      query = QueryBuilder.savestm(ent, sTypes)
-      return done(null, {query: query, operation: 'save'})
-    }
-
-    seneca.act({role: actionRole, hook: 'generate_id', target: args.target}, function (err, result) {
-      if (err) {
-        seneca.log.error('hook generate_id failed')
-        return done(err)
-      }
-      ent.id = result.id
-      query = QueryBuilder.savestm(ent, sTypes)
-      return done(null, {query: query, operation: 'save'})
-    })
-  })
-
   seneca.add({role: actionRole, hook: 'load'}, function (args, done) {
     var qent = args.qent
     var sTypes = specificTypes(args.target)
@@ -108,14 +74,5 @@ module.exports = function queryBuilder (options) {
         }
       }
     }
-  })
-
-  seneca.add({role: actionRole, hook: 'remove'}, function (args, done) {
-    var qent = args.qent
-    var q = args.q
-    var sTypes = specificTypes(args.target)
-
-    var query = QueryBuilder.deletestm(qent, q, sTypes)
-    return done(null, {query: query})
   })
 }
