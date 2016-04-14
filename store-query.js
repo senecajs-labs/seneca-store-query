127f97e1244875b9b7588e90cfb67b1634f7bc3d
Mihai Dima <mihaizn@gmail.com>
2016-04-14 13:36:58 +0300

    reuse code from standard query

--- store-query.js
+++ store-query.js
@@ -35,45 +35,13 @@ module.exports = function queryBuilder (options) {
   })
 
   seneca.add({role: actionRole, hook: 'list'}, function (args, done) {
-    var qent = args.qent
     var q = args.q
+    var qent = args.qent
     var sTypes = specificTypes(args.target)
 
-    buildSelectStatement(q, sTypes, function (err, query) {
+    QueryBuilder.buildSelectStatement(qent, q, sTypes, function (err, query) {
       return done(err, {query: query})
     })
-
-    function buildSelectStatement (q, sTypes, done) {
-      var query
-
-      if (_.isString(q)) {
-        return done(null, q)
-      }
-      else if (_.isArray(q)) {
-        // first element in array should be query, the other being values
-        if (q.length === 0) {
-          var errorDetails = {
-            message: 'Invalid query',
-            query: q
-          }
-          seneca.log.error('Invalid query')
-          return done(errorDetails)
-        }
-        query = {}
-        query.text = QueryBuilder.fixPrepStatement(q[0], sTypes)
-        query.values = _.clone(q)
-        query.values.splice(0, 1)
-        return done(null, query)
-      }
-      else {
-        if (q.ids) {
-          return done(null, QueryBuilder.selectstmOr(qent, q, sTypes))
-        }
-        else {
-          QueryBuilder.selectstm(qent, q, sTypes, done)
-        }
-      }
-    }
   })
 
   return {
